2159bf26363b071d4584ec17a90ec5d5e5170e9e
Shikha-code36 <pandeyshikha075@gmail.com>
2023-02-13 20:02:50 +0530

    updation in contants

--- projectconstant.py
+++
@@ -1,9 +0,0 @@
-MONGO_STRING = 'mongodb://localhost:27017/'
-APPSECRET = 'super secret key'
-SOURCE = 'smtp.gmail.com'
-APIKEY_EMAIL = 'jzvisfhwpsmnvurz'
-EMAIL = 'shikhapyai@gmail.com'
-PHONENO = '+19299306828'
-SMS_API_SSID = 'ACf1336b2d0ff3a8124f59ac1f06b8ae7a'
-SMS_URL = 'https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json'.format(SMS_API_SSID)
-SMS_API_KEY = '936222beb289fe27cddb48b3054c7368'
